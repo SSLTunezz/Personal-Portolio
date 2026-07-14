bd99dc7583d4123b07570e21ec9e96cad9fc9ffb
SSLTunezz <natiiksingh.jsr@gmail.com>
2026-07-14 18:05:48 +0200

    Finshed the INdex.css theme's and style by adding some general rules like html, *  and body making sure the scroll is smooth, the border is fine and the background is fine. Also added some #root settings #root {   max-width: 100%;   margin: 0 auto;   padding: 0;   text-align: center; }
    Additionally finally created LIGHT and Dark Mode! WITH LOCAL STORAGE :YIPEE

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,11 @@
+import {ThemeToggle} from "@/Components/ThemeToggle.jsx";
 
 export const Home = () => {
 
-    return <div className="min-h-screen ">
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
         {/* Theme Toggle */}
-
+        <ThemeToggle />
         {/* Background Effects */}
 
         {/* Navbar */}
