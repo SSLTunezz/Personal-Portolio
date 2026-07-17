e6dabc5df2770164632478919f6cc746b1f88eaa
SSLTunezz <natiiksingh.jsr@gmail.com>
2026-07-17 10:07:00 +0200

    Added a nav bar which has links to div's such as about me etc.. but I haven't made the div themselves YET. Meteors added in background and for the nav bar had to resize based on my screen and found out buttons overlap each other which isn't good. Added name to the top right which a href redirect. Nav bar should have blurred background when scrolling but not tested yet

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,56 @@
+import {cn} from "@/lib/utils.js";
+import {useEffect, useState} from "react";
+
+const navItems = [
+    {name: "Home", href: "#hero"},
+    {name: "About", href: "#about"},
+    {name: "Skills", href: "#skills"},
+    {name: "Projects", href: "#projects"},
+    {name: "Contact", href: "#contact"},
+]
+
+
+
+export const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.screenY > 10)
+        };
+
+        window.addEventListener("scroll", handleScroll)
+        return () => window.removeEventListener("scroll", handleScroll);
+
+    })
+
+
+    return (
+    <nav className={cn("fixed w-2/4 z-40 transition-all duration-300",
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5",
+        )}
+    >
+        <div className="container flex items-center justify-between ">
+            <a className="text-xl font-bold text-primary flex items-center"
+               href="#hero">
+                <span className="relative z-10">
+                    {" "}
+                    <span className="text-glow text-foreground">  Naitik Singh's </span> portfolio
+                </span>
+            </a>
+
+            {/* desktop nav */}
+            <div className="hidden md:flex space-x-10 ">
+                {navItems.map((item, key) => (
+                    <a href={item.href} key={key}>
+                        {item.name}
+                    </a>
+                    ))}
+            </div>
+
+        </div>
+
+
+    </nav>
+    )
+};
