84f0317c8bfea1e4e6397a5e168367130e3f4ca4
SSLTunezz <natiiksingh.jsr@gmail.com>
2026-07-28 09:26:17 +0200

    Added a projects sectio with 4 boxes, flex collumn. Learned what flex collums acc are, they are cool but its going to be a pain when I have new project I want to show lol...

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,35 @@
+
+
+export const ProjectSection = () => {
+    return (
+        <section
+            id="projects"
+            className={"py-24 px-4 relative"}>
+            {""}
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center ">
+                    Showcase of my projects
+                </h2>
+            </div>
+            <div className={"container mx-auto max-w-10xl flex flex-col flex-row md:flex-row gap-12 justify center items-center"}>
+                <div className={"box"}>
+                      text
+                 </div>
+                <div className={"box"}>
+                    text 2
+                </div>
+                <div className={"box"}>
+                    text 3
+                </div>
+                <div className={"box"}>
+                    text 4
+                </div>
+            </div>
+            <div className="container mx-auto max-w-5xl">
+                <p>
+                    So yayayay
+                </p>
+            </div>
+        </section>
+    )
+}
